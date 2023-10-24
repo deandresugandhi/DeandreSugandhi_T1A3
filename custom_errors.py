f9a4a6285f86ec488be7a05ac7d6469a9d30116f
deandresugandhi <deandre_21@hotmail.com>
2023-10-25 01:03:25 +1100

    Working on adding comments for readability, and using PEP8 to meet style requirements

--- custom_errors.py
+++ custom_errors.py
@@ -1,3 +1,15 @@
 class ColumnFullError(Exception):
     def __init__(self, message):
         super().__init__(message)
+
+class UsernameError(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+
+class ColorError(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+
+class PieceTypeError(Exception):
+    def __init__(self, message):
+        super().__init__(message)
--- game_board.py
+++ game_board.py
@@ -1,65 +1,118 @@
+import re
 import numpy as np
 from termcolor import colored
-import re
 import art
 from custom_errors import ColumnFullError
 
 class Piece:
+    """
+    Represents a game piece used in the game.
+    
+    The game piece is a visualization of a player's piece on the board. The
+    appearance can be customized by changing a Piece instance's attributes.
+
+    Attributes:
+    1. _player_name (str): The name of the player owning the piece.
+    2. _color (str): The color of the piece.
+    3. _piece_type (str): The visual representation of the piece
+       (i.e. its shape)
+    4. _player (Player): The player number of the piece (i.e. p1 or p2)
+    5. surrender (bool): Represents whether the owner of the piece surrendered.
+    """
     def __init__(self, player_name, color, piece_type, player):
         self._player_name = player_name
         self._color = color
         self._piece_type = piece_type
         self._player = player
         self.surrender = False
-    
+
+
     @property
     def player_name(self):
+        """A method to access the player's name."""
         return self._player_name
-    
+
     @player_name.setter
-    def player_name(self, player_name): 
-        if re.fullmatch("^[a-zA-Z0-9][a-zA-Z0-9._-]{3,18}[a-zA-Z0-9]$", player_name):
+    def player_name(self, player_name):
+        """
+        A method to set the player's name. It includes validation to make sure
+        it is in the right format (alphanumeric, periods, and underscores only,
+        5-20 characters long, name must start and end with an alphanumeric 
+        character). 
+        
+        Raises custom UsernameError if format is not followed.  
+        """
+        if re.fullmatch("^[a-zA-Z0-9][a-zA-Z0-9._-]{3,18}[a-zA-Z0-9]$",
+            player_name):
             self._player_name = player_name
         else:
-            raise ValueError("Invalid name.")
-        
+            raise UsernameError("Invalid player name.")
+
     @property
     def color(self):
+        """A method to access the piece color."""
         return self._color.lower()
-    
+
     @color.setter
     def color(self, color):
+        """
+        A method to set the piece color. Termcolor module's colored() function
+        is used to test the value assigned as the _color attribute. Termcolor
+        will raise KeyError if it fails to set the value as color to the string.
+
+        Raises ColorError if KeyError is raised by termcolor module. If no error
+        is raised, the _color attribute is set to the value of color in lower
+        case for consistency.
+        """
         try:
             colored("test", color)
-        except KeyError:
-            raise ValueError("Invalid color.")
-        else:
-            self._color = color.lower()
-    
+        except KeyError as exc:
+            raise ColorError("Invalid color.") from exc
+        self._color = color.lower()
+
     @property
     def piece_type(self):
+        """A method to access the piece type."""
         return self._piece_type
-    
+
     @piece_type.setter
     def piece_type(self, piece_type):
+        """
+        A method to set the piece type. It includes validation to make sure
+        it is in the right format (alphanumeric and 1 character long). 
+        
+        Raises custom PieceTypeError if format is not followed.
+        """
         if re.fullmatch("^[a-zA-Z0-9]$", piece_type):
             self._piece_type = piece_type
         else:
-            raise ValueError("Invalid piece type. Please try again.")
+            raise PieceTypeError("Invalid piece type. Please try again.")
 
     @property
     def player(self):
+        """A method to access the piece's player number."""
         return self._player
 
     @property
     def surrender(self):
+        """A method to access the piece's player's surrender state."""
         return self._surrender
-    
+
     @surrender.setter
     def surrender(self, surrender):
+        """A method to set the piece's players's surrender state."""
         self._surrender = surrender
 
     def drop(self, board, column):
+        """
+        A method to drop the piece onto the board.
+
+        Args:
+        1. board (Board): An instance of the Board class, defining the board 
+           onto which the piece will be dropped.
+        2. column (str): A string digit representing the column number of the
+           board to drop the piece onto
+        """
         row = -1
         while board._array[row, column - 1] != "0":
             row -= 1
@@ -69,6 +122,25 @@ class Piece:
 
 
 class Board:
+    """
+    Represents a game board used in the game.
+    
+    The game board is a 6 x 7 Connect Four board. It functions as a NumPy array
+    which is translated and displayed on the terminal through various methods.
+
+    Attributes:
+    1. _array (np.array): A NumPy array with 6 lists (representing rows) with
+       7 elements each (representing columns).
+    2. _edge (str): Component used to display the board. It is the bottom edge 
+       of the game board, representing the slider to clear the board.
+    3. _divider (str): Component used to display the board. Represents the 
+       dividers between each row of the game board.
+    4. _column (list): A list of components to display the board. Represents
+       each "cage" of the board, i.e. the columns.
+    5. players (list): A list of Piece class instances, defining the pieces and
+       players that is playing on the board, accessing its properties such as
+       piece color, piece type, player name, etc to be displayed by the board.
+    """
     def __init__(self, players):
         self._array = np.array([["0" for i in range(7)] for i in range(6)])
         self._edge = "+===" *7 + "+"
@@ -78,23 +150,34 @@ class Board:
 
     @property
     def players(self):
+        """A method to access the board's players, a list of Piece instances."""
         return self._players
-    
+
     @players.setter
     def players(self, players):
+        """A method to set the board's players, a list of Piece instances."""
         self._players = players
 
     @property
     def array(self):
+        """A method to access the board's array."""
         return self._array
 
+    @array.setter
+    def array(self, array):
+        """A method to set the board's array."""
+        self._array = array
+
     def display(self):
+        """A method to display the board array's visual representation."""
+        # Sets a dictionary as a reference to display 
         piece_dict = {
             "0": " ",
-            "1": colored(self._players[0].piece_type, self._players[0].color),
-            "2": colored(self._players[1].piece_type, self._players[1].color),
+            "1": colored(self.players[0].piece_type, self.players[0].color),
+            "2": colored(self.players[1].piece_type, self.players[1].color),
         }
         logo = colored(art.text2art(f"CONNECT 4", font="small", space = 0), "light_grey")
+        
         print(logo[0:-10])
         print(f"{self._players[0].player_name} = {piece_dict.get('1')}".center(58))
         print(f"{self._players[1].player_name} = {piece_dict.get('2')}".center(58))
@@ -112,4 +195,4 @@ class Board:
         print(self._edge.center(50))
 
     def clear_board(self):
-        self._array = np.array([["0" for i in range(7)] for i in range(6)])
+        self.array = np.array([["0" for i in range(7)] for i in range(6)])
