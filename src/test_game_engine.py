264f16759952a16812591d4ed86d7a3607f7fb70
deandresugandhi <deandre_21@hotmail.com>
2023-10-29 11:39:20 +1100

    Minor addition to installation guide, updated test to include diagonal victory, updated test_report.txt

--- src/test_game_engine.py
+++ src/test_game_engine.py
@@ -9,7 +9,7 @@ from game_board import Board, Piece
 from win_conditions import VictoryChecker
 
 
-def test_game_in_progress_p1wins(monkeypatch):
+def test_game_in_progress_p1wins_vertical_victory(monkeypatch):
     """
     Tests the following sequence during game_in_progress: player 1 putting
     pieces in column 1 during their turn and player 2 putting pieces in column
@@ -24,6 +24,58 @@ def test_game_in_progress_p1wins(monkeypatch):
 
     # Sequence of user input to achieve the desired sequence of commands
     input_sequence = iter(["1", "2", "1", "2","1", "2", "1"])
+
+    # Replaces default input function to enter input_sequence in sequence
+    monkeypatch.setattr("builtins.input", lambda _: next(input_sequence))
+    
+    # Main function
+    result = game_in_progress(board, players, referee)
+
+    # Function should return player 1 as a winner, with no surrendered players
+    assert result == (player1, None)
+
+
+def test_game_in_progress_p1wins_horizontal_victory(monkeypatch):
+    """
+    Tests the following sequence during game_in_progress: players 1 and 2
+    putting their pieces on columns 1 - 4 alternatingly. This should result in 
+    player 1 horizontal victory.
+    """
+    # Define required variables
+    player1 = Piece("Test1","red", "O", 1)
+    player2 = Piece("Test2","blue", "X", 2)
+    players = [player1, player2]
+    board = Board(players)
+    referee = VictoryChecker(board, players)
+
+    # Sequence of user input to achieve the desired sequence of commands
+    input_sequence = iter(["1", "1", "2", "2","3", "3", "4"])
+
+    # Replaces default input function to enter input_sequence in sequence
+    monkeypatch.setattr("builtins.input", lambda _: next(input_sequence))
+    
+    # Main function
+    result = game_in_progress(board, players, referee)
+
+    # Function should return player 1 as a winner, with no surrendered players
+    assert result == (player1, None)
+
+
+def test_game_in_progress_p1wins_diagonal_victory(monkeypatch):
+    """
+    Tests the following sequence during game_in_progress: players 1 and 2
+    taking turns to ensure player 1 wins with a diagonal victory from the following
+    slots (row, column): (1, 1), (2, 2), (3, 3), (4, 4).
+    """
+    # Define required variables
+    player1 = Piece("Test1","red", "O", 1)
+    player2 = Piece("Test2","blue", "X", 2)
+    players = [player1, player2]
+    board = Board(players)
+    referee = VictoryChecker(board, players)
+
+    # Sequence of user input to achieve the desired sequence of commands
+    input_sequence = iter(["1", "2", "2", "3", "4", "3", "3", "4", "5", "4", "4"])
 
     # Replaces default input function to enter input_sequence in sequence
     monkeypatch.setattr("builtins.input", lambda _: next(input_sequence))
